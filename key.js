fc8a2b557c35fb3973e1ba8e95e2e71659968472
pedrofelippe <pedfelippe@gmail.com>
2020-04-30 23:15:58 -0300

    Criaçao do Readme

--- key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: "fuSo0T86Il3iI6Q1iKKBky8DH",
-    consumer_secret: "xKhST3ewPHAzsvTMqvBZogR6LMxTOtwtR6HxuUrBTcLix34yit",
-    access_token_key: "850860360693415937-i4hL5jf4omebLWnxyx0VJCljqo9l8Bx",
-    access_token_secret: "WiCdAQ3uhXnvpaWuZcuEnSYquwwAMExNa41jLEwIq8KiM"
-}
