7ae227ed947dc2cc91f11d9a50ce6a72a5a57437
Pedro Felippe <pedfelippe@gmail.com>
2020-05-07 15:45:12 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: "3j6ejJtPMAlQrYwjULSdksyTc",
-    consumer_secret: "AKgqQK6w5oDArwuHVVzgiAMRXgPWcq01gh0UWKlxHHv4DHPvUO",
-    access_token_key: "850860360693415937-a8WMQz5g5uE1HsPm2v8k1ZEUBZ3ZtU7",
-    access_token_secret: "GvNDfBvc6cvHsGI5ZOz3GExnseI1UHhudX1zKezdWMAgw"
+    consumer_key: "sua-key",
+    consumer_secret: "sua-key",
+    access_token_key: "sua-key-sua-key",
+    access_token_secret: "sua-key"
 }
