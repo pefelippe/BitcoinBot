733df38c15cb3c7fb14f482d4df4b65060edeb9f
pedrofelippe <pedfelippe@gmail.com>
2020-05-07 15:46:05 -0300

    teste

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: "3j6ejJtPMAlQrYwjULSdksyTc",
-    consumer_secret: "AKgqQK6w5oDArwuHVVzgiAMRXgPWcq01gh0UWKlxHHv4DHPvUO",
-    access_token_key: "850860360693415937-a8WMQz5g5uE1HsPm2v8k1ZEUBZ3ZtU7",
-    access_token_secret: "GvNDfBvc6cvHsGI5ZOz3GExnseI1UHhudX1zKezdWMAgw"
-}
